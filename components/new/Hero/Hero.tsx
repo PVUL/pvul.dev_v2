c5789ab524264d3e81d5dd4a86651a985cbd8264
Paul Yun <paul.a.yun@gmail.com>
2023-01-26 16:41:55 -0800

    Remove framer motion on hero text

--- components/new/Hero/Hero.tsx
+++ components/new/Hero/Hero.tsx
@@ -18,17 +18,14 @@ export const Hero = () => (
         layers={[{ image: 'banner.png', speed: -25 }]}
         className={styles.banner}
       >
-        <motion.div
-          variants={textVariant(0.15)}
-          className="absolute inset-0 flex flex-col justify-center p-8 text-right sm:p-32"
-        >
+        <div className="absolute inset-0 flex flex-col justify-center p-8 text-right sm:p-32">
           <h1 className="mb-8 font-thin leading-none text-white text-7xl">
             I design to make people's dreams come true.
           </h1>
           <p className={styles.text}>
             / web design <br /> / app development <br />/ product design
           </p>
-        </motion.div>
+        </div>
       </ParallaxBanner>
     </motion.div>
   </AnimatePresence>
