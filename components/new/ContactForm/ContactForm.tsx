d518fdb613e5eaea0990ec07f37465bd07a00725
Paul Yun <paul.a.yun@gmail.com>
2023-01-27 16:16:04 -0800

    Update styles for contact submission

--- components/new/ContactForm/ContactForm.tsx
+++ components/new/ContactForm/ContactForm.tsx
@@ -35,7 +35,7 @@ export const ContactForm = () => {
   const { register, reset, handleSubmit, formState } = useForm<Inputs>({
     resolver: yupResolver(schema),
   })
-  const { errors, isSubmitted } = formState
+  const { errors, isSubmitted, isSubmitting } = formState
 
   useEffect(() => {
     if (formState.isSubmitSuccessful) {
@@ -58,7 +58,8 @@ export const ContactForm = () => {
     toast.custom((t) => <Toaster t={t} message={message} />, {
       id: 'successfulContact',
       position: 'top-right',
-      duration: 6000,
+      duration: 106000,
+      // duration: 6000,
     })
   }
 
@@ -113,7 +114,7 @@ export const ContactForm = () => {
         <input
           disabled={!!errors.email || !!errors.name || !!errors.message}
           type="submit"
-          value="send"
+          value={isSubmitting ? 'sending...' : 'send'}
           className="mt-2 btn btn-block"
         />
       </form>
