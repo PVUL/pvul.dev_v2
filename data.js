ee2eab09d33a73745568e9b8926b3dcdca01b2e2
Paul Yun <paul.a.yun@gmail.com>
2023-02-03 23:47:36 -0800

    Add new works

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ export const works = [
     description:
       'Almanac is a document sharing platform for teams. While working on this project, it was important to me to have a consistent style across all parts of the customer experience. I created the home page, registration, profiles, and library components- such as navbar, article cards, and avatars. Optimized all pages for SEO.',
     technologies:
-      'React, Typescript, Rails, Ant Design, Heroku, Cloudinary, Algolia, Figma',
+      'React, Typescript, Ruby on Rails, Ant Design, Heroku, Cloudinary, Algolia, Figma',
     images: [
       {
         alt: 'almanac-1',
@@ -29,7 +29,8 @@ export const works = [
     link: '',
     description:
       'Wayfair Registry is a wedding registry platform for Wayfair. While working on this project, the most important thing was make features that would convert towards higher conversion rates, which were measured through A-B testing. Created the homepage, brand pages, gift tracking, and a registry referral program. Improved page performance by 50% by optimizing images above-the-fold. Also, created documentation for other team members.',
-    technologies: 'React, PHP, MSSQL, Redis, GraphQL, Invision',
+    technologies:
+      'React, Javascript, PHP, SASS, SQL Server, Redis, GraphQL, Invision',
     images: [
       {
         alt: 'wayfair-1',
@@ -42,6 +43,101 @@ export const works = [
       {
         alt: 'wayfair-3',
         source: '/works/wayfair-3.png',
+      },
+    ],
+  },
+  {
+    title: 'Giyea',
+    link: 'https://giyea.com',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Iaculis at erat pellentesque adipiscing commodo elit at. Egestas quis ipsum suspendisse ultrices gravida. Mi proin sed libero enim sed faucibus turpis. At lectus urna duis convallis convallis tellus id interdum velit. Sed vulputate odio ut enim blandit volutpat. Est ante in nibh mauris cursus mattis molestie. Pellentesque nec nam aliquam sem et tortor consequat id porta. Ut tristique et egestas quis ipsum suspendisse ultrices gravida. Vitae et leo duis ut diam. Nec ultrices dui sapien eget mi proin. At lectus urna duis convallis convallis tellus. Viverra nam libero justo laoreet.',
+    technologies: 'Illustrator',
+    images: [
+      {
+        alt: 'giyea-1',
+        source: '/works/giyea-1.png',
+      },
+      {
+        alt: 'giyea-2',
+        source: '/works/giyea-2.png',
+      },
+      {
+        alt: 'giyea-3',
+        source: '/works/giyea-3.png',
+      },
+    ],
+  },
+  {
+    title: 'Pavlok',
+    link: '',
+    description:
+      'Urna neque viverra justo nec. Placerat vestibulum lectus mauris ultrices. Egestas sed tempus urna et. Lacus viverra vitae congue eu consequat ac felis. Tortor aliquam nulla facilisi cras fermentum. Integer feugiat scelerisque varius morbi enim nunc faucibus. In cursus turpis massa tincidunt. Tincidunt dui ut ornare lectus sit amet. Adipiscing elit ut aliquam purus sit amet luctus. Scelerisque viverra mauris in aliquam sem. Pulvinar mattis nunc sed blandit libero volutpat. A lacus vestibulum sed arcu non odio euismod lacinia at. Ut lectus arcu bibendum at varius vel pharetra. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Ultrices vitae auctor eu augue ut lectus arcu. Felis donec et odio pellentesque diam volutpat commodo.',
+    technologies:
+      'Solidworks, Keyshot, Photoshop, Illustrator, Indesign, 3d printing',
+    images: [
+      {
+        alt: 'pavlok-1',
+        source: '/works/pavlok-1.png',
+      },
+      {
+        alt: 'pavlok-2',
+        source: '/works/pavlok-2.png',
+      },
+    ],
+  },
+  {
+    title: 'Aila',
+    link: '',
+    description:
+      'Urna neque viverra justo nec. Placerat vestibulum lectus mauris ultrices. Egestas sed tempus urna et. Lacus viverra vitae congue eu consequat ac felis. Tortor aliquam nulla facilisi cras fermentum. Integer feugiat scelerisque varius morbi enim nunc faucibus. In cursus turpis massa tincidunt. Tincidunt dui ut ornare lectus sit amet. Adipiscing elit ut aliquam purus sit amet luctus. Scelerisque viverra mauris in aliquam sem. Pulvinar mattis nunc sed blandit libero volutpat. A lacus vestibulum sed arcu non odio euismod lacinia at. Ut lectus arcu bibendum at varius vel pharetra. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Ultrices vitae auctor eu augue ut lectus arcu. Felis donec et odio pellentesque diam volutpat commodo.',
+    technologies:
+      'Solidworks, Keyshot, Photoshop, Illustrator, 3d printing, Laser Cutting',
+    images: [
+      {
+        alt: 'aila-1',
+        source: '/works/aila-1.png',
+      },
+      {
+        alt: 'aila-2',
+        source: '/works/aila-2.png',
+      },
+    ],
+  },
+  {
+    title: 'Escargot',
+    link: '',
+    description:
+      'Urna neque viverra justo nec. Placerat vestibulum lectus mauris ultrices. Egestas sed tempus urna et. Lacus viverra vitae congue eu consequat ac felis. Tortor aliquam nulla facilisi cras fermentum. Integer feugiat scelerisque varius morbi enim nunc faucibus. In cursus turpis massa tincidunt. Tincidunt dui ut ornare lectus sit amet. Adipiscing elit ut aliquam purus sit amet luctus. Scelerisque viverra mauris in aliquam sem. Pulvinar mattis nunc sed blandit libero volutpat. A lacus vestibulum sed arcu non odio euismod lacinia at. Ut lectus arcu bibendum at varius vel pharetra. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Ultrices vitae auctor eu augue ut lectus arcu. Felis donec et odio pellentesque diam volutpat commodo.',
+    technologies: 'Solidworks, Keyshot, Photoshop, Illustrator, 3d printing',
+    images: [
+      {
+        alt: 'spixi-1',
+        source: '/works/spixi-1.png',
+      },
+      {
+        alt: 'spixi-2',
+        source: '/works/spixi-2.png',
+      },
+      {
+        alt: 'spixi-3',
+        source: '/works/spixi-3.png',
+      },
+    ],
+  },
+  {
+    title: 'Ergotech',
+    link: '',
+    description:
+      'Urna neque viverra justo nec. Placerat vestibulum lectus mauris ultrices. Egestas sed tempus urna et. Lacus viverra vitae congue eu consequat ac felis. Tortor aliquam nulla facilisi cras fermentum. Integer feugiat scelerisque varius morbi enim nunc faucibus. In cursus turpis massa tincidunt. Tincidunt dui ut ornare lectus sit amet. Adipiscing elit ut aliquam purus sit amet luctus. Scelerisque viverra mauris in aliquam sem. Pulvinar mattis nunc sed blandit libero volutpat. A lacus vestibulum sed arcu non odio euismod lacinia at. Ut lectus arcu bibendum at varius vel pharetra. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Ultrices vitae auctor eu augue ut lectus arcu. Felis donec et odio pellentesque diam volutpat commodo.',
+    technologies: 'Solidworks, Keyshot, Photoshop, Illustrator',
+    images: [
+      {
+        alt: 'versastand-1',
+        source: '/works/versastand-1.png',
+      },
+      {
+        alt: 'versastand-2',
+        source: '/works/versastand-2.png',
       },
     ],
   },
