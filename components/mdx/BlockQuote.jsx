4734781aaff06865f82814fdbe396d37fab05014
Paul Yun <paul.a.yun@gmail.com>
2023-03-15 17:27:10 -0700

    💄 Add styles for blockquote

--- components/mdx/BlockQuote.jsx
+++ components/mdx/BlockQuote.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { ImQuotesLeft as QuoteIcon } from 'react-icons/im'
+
+// NOTE: this would be a fun custom component to create
+// taking in a quote, author, and source, that way we
+// don't need a separate figcaption and nested cite element
+// in the mdx file, for example:
+//
+// > this is the quote.
+//
+// <figcaption>
+//   —Leonardo Da Vinci,
+//   <cite>Walter Issacson</cite>
+// </figcaption>
+//
 
 export const BlockQuote = ({ children }) => (
-  <blockquote className="relative p-10 pb-8 m-auto text-xl italic w-96 text-neutral-600 bg-slate-100">
-    <div
-      className="absolute top-0 mt-4 leading-none text-8xl text-neutral-300"
+  <blockquote className="relative m-auto text-xl italic sm:max-w-[40rem] text-neutral-700">
+    <QuoteIcon
+      className="absolute top-0 mt-4 ml-3 leading-none sm:ml-10 text-7xl text-neutral-200"
       aria-hidden="true"
-    >
-      &ldquo;
-    </div>
-    <div className="mt-10">{children}</div>
+    />
+    <div className="relative px-8 text-3xl p-14 sm:px-14">{children}</div>
   </blockquote>
 )
 
