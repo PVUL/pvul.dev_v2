fe9426b18c6179a245f7982e1672107051fcfdc7
Paul Yun <paul.a.yun@gmail.com>
2023-03-25 22:27:55 -0700

    💄 Admin preview fixes

---
+++ components/BlogPostPreview.jsx
@@ -0,0 +1,4 @@
+// see https://zslabs.com/articles/mdx-previews-in-netlify-cms
+import { MdxPreview } from './MdxPreview'
+
+export const BlogPostPreview = ({ entry }) => <MdxPreview entry={entry} />
