0ac38ff3d16d18a607083fc1ab7ef3b48ef20ebc
Paul Yun <paul.a.yun@gmail.com>
2023-03-31 10:50:21 -0700

    💄 Add socials to nav drawer

--- components/new/Footer/Footer.tsx
+++ components/new/Footer/Footer.tsx
@@ -1,11 +1,8 @@
-import {
-  RiGithubFill as GithubIcon,
-  RiLinkedinBoxFill as LinkedinIcon,
-  RiTwitterFill as TwitterIcon,
-} from 'react-icons/ri'
 import Link from 'next/link'
 
 import styles from './Footer.module.scss'
+import { SocialLinks } from '../SocialLinks'
+import { MadeWithLove } from '../MadeWithLove'
 
 export const Footer = () => (
   <div className={styles.footer}>
@@ -30,40 +27,7 @@ export const Footer = () => (
       could have a full, happy, and contented life.
     </p>
 
-    <div className={styles.socials}>
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://github.com/pvul"
-        className="transition-opacity hover:opacity-75"
-        aria-label="github"
-      >
-        <GithubIcon className={styles.icon} />
-      </a>
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://twitter.com/__pvul"
-        className="transition-opacity hover:opacity-75"
-        aria-label="github"
-      >
-        <TwitterIcon className={styles.icon} />
-      </a>
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://linkedin.com/in/yunpaul"
-        className="transition-opacity hover:opacity-75"
-        aria-label="linkedin"
-      >
-        <LinkedinIcon className={styles.icon} />
-      </a>
-    </div>
-
-    <p className={styles.text}>
-      Made with {'<3'} by Paul Yun
-      <br />
-    </p>
-    <p className={styles.text}>©2023</p>
+    <SocialLinks />
+    <MadeWithLove />
   </div>
 )
